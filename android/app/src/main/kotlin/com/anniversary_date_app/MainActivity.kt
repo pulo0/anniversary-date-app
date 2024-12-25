b654af8bf96f25b62ce0a5ab46a96943123f79cf
pulo0 <bartexx13042005@gmail.com>
2024-12-25 12:00:27 +0100

    feat: changed application name (on Android) and initiated Firebase for future use

--- android/app/src/main/kotlin/com/anniversary_date_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/anniversary_date_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.anniversary_date_app
+package com.anniversary_date_app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:anniversary_date_app/style/app_theme.dart';
 import 'package:anniversary_date_app/presentation/tab/tab_screen.dart';
 import 'package:anniversary_date_app/data/service/service_locator.dart';
+import 'package:anniversary_date_app/data/service/firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   setupLocator();
   runApp(const MainApp());
 }
